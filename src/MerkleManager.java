9144b3a1f97c467e4ea3882ca09c0d0c79ed230e
cesarcuadro <cesarcuadro777@gmail.com>
2024-09-01 13:10:58 -0700

    set instance variables

--- src/MerkleManager.java
+++ src/MerkleManager.java
@@ -1,3 +1,6 @@
 public class MerkleManager {
-    // Cesar
+    public static volatile String userInput;
+    public static String expectedUserOutput;
+    public static String merkleRoot = null;
+    public static int strikes = 0;
 }
