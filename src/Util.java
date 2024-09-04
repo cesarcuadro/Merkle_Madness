4bffb642033d697ebb4c4cd1c9bf15300a592feb
vihuynh72 <165195947+vihuynh72@users.noreply.github.com>
2024-09-03 17:59:17 -0700

    Update Util.java

--- src/Util.java
+++ src/Util.java
@@ -1,4 +1,54 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+
 public class Util {
     // Vi
 
+    public String getMerkleRoot(ArrayList<String> lstItems) {
+        MerkleNode oNode1 = new MerkleNode();
+        MerkleNode oNode2 = new MerkleNode();
+        MerkleNode oNode3 = new MerkleNode();
+        MerkleNode oNode4 = new MerkleNode();
+        MerkleNode oNode5 = new MerkleNode();
+        MerkleNode oNode6 = new MerkleNode();
+        MerkleNode oNode7 = new MerkleNode();
+
+        oNode1.sHash = generateHash(lstItems.get(0));
+        oNode2.sHash = generateHash(lstItems.get(1));
+        oNode3.sHash = generateHash(lstItems.get(2));
+        oNode4.sHash = generateHash(lstItems.get(3));
+
+        populateMerkleNode(oNode5, oNode1, oNode2);
+        populateMerkleNode(oNode6, oNode3, oNode4);
+        populateMerkleNode(oNode7, oNode5, oNode6);
+
+        return oNode7.sHash;
+    }
+
+    private void populateMerkleNode(MerkleNode oNode, MerkleNode oLeftNode, MerkleNode oRightNode) {
+
+        oNode.oLeft = oLeftNode;
+        oNode.oRight = oRightNode;
+        oNode.sHash = generateHash(oNode.oLeft.sHash + oNode.oRight.sHash);
+    }
+
+    public synchronized String generateHash(String sOriginal) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] btEncodedhash = digest.digest(
+                    sOriginal.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < btEncodedhash.length; i++) {
+                sb.append(Integer.toString((btEncodedhash[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+        } catch (Exception ex) {
+
+            System.out.println("Error generating hash: " + ex.getMessage());
+            return null;
+        }
+    }
 }
