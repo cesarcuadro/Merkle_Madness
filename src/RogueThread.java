fc7be5d43db068e2f244faea32ff009165156f8a
vihuynh72 <165195947+vihuynh72@users.noreply.github.com>
2024-09-03 18:35:07 -0700

    Finish RogueThread.java

--- src/RogueThread.java
+++ src/RogueThread.java
@@ -1,3 +1,15 @@
-public class RogueThread {
-    // Rodolfo
+public class RogueThread implements Runnable {
+
+    public void run() {
+        Util oUtil = new Util();
+        while (true) {
+            oUtil.sleepRandomTime("Rogue");
+            String sNewWord = MerkleManager.grabWord();
+            if (sNewWord != null) {
+                MerkleManager.strikes++;
+                System.out.println("Rogue grabbed a sword and mentioned \"STRIKE\"");
+                }
+        }
+
+    }
 }
