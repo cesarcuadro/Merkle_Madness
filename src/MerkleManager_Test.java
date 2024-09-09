f84e6e01fbe94fa992662ada16c0d811aa51c9e9
cesarcuadro <106943802+cesarcuadro@users.noreply.github.com>
2024-09-09 14:52:42 -0700

    Update MerkleManager_Test.java

--- src/MerkleManager_Test.java
+++ src/MerkleManager_Test.java
@@ -1,5 +1,6 @@
 public class MerkleManager_Test {
-    // Rodolfo
+    // Vi Hyunh
+    // Cesar Cuadro
     public static void main(String[] args) {
         MerkleManager m = new MerkleManager();
         m.manage();
